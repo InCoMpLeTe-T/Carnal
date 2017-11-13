efd2d62825af7adbb8fd6f00abc662b5c3c28d88
Benjamin Sandland <benjaminsandland@gmail.com>
2017-11-13 10:45:47 +0100

    Initializing project

---
+++ IsEven.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Please enter a number");
+            string input = Console.ReadLine();
+            Int32.TryParse(input, out num);
+
+            if(num % 2 == 0) // Modulus to check if even
+            {
+                Console.WriteLine("The number" + num + " is even");
+                Console.ReadKey();
+            }
+            else // If num isn't even, must be false
+            {
+                Console.WriteLine("The number " + num + " is even");
+                Console.ReadKey();
+            }
+        }
+    }
+}
